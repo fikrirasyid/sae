5476d6b205303994eb118d99eb9018158f17168c
Fikri Rasyid <fikrirasyid@gmail.com>
2019-01-08 06:31:59 +0700

    Initial commit from Create React App

---
+++ __plugin_name.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Sae
+Plugin URI:  
+Description: Beautiful gallery and progressive image modules for Divi
+Version:     1.0.0
+Author:      Fikri Rasyid
+Author URI:  http://fikrirasy.id
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: sae-sae
+Domain Path: /languages
+
+Sae is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Sae is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Sae. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+
+if ( ! function_exists( 'sae_initialize_extension' ) ):
+/**
+ * Creates the extension's main class instance.
+ *
+ * @since 1.0.0
+ */
+function sae_initialize_extension() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/Sae.php';
+}
+add_action( 'divi_extensions_init', 'sae_initialize_extension' );
+endif;
---
+++ includes/__PluginName.php
@@ -0,0 +1,46 @@
+<?php
+
+class SAE_Sae extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'sae-sae';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'sae';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * SAE_Sae constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'sae', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new SAE_Sae;
