a2a51b22daca223c754c1cef0a2981864cffefed
Fikri Rasyid <fikrirasyid@gmail.com>
2019-01-13 20:59:31 +0700

    Abstracting gallery data attributes and classname

--- includes/modules/Gallery/Gallery.php
+++ includes/modules/Gallery/Gallery.php
@@ -69,8 +69,38 @@ class SAE_Gallery extends ET_Builder_Module {
 	}
 
 	public function render( $attrs, $content = null, $render_slug ) {
+		// Wrapper classnames
+		$wrapper_classnames = array(
+			'sae-gallery-wrapper',
+			"sae-gallery-layout-{$this->props['layout']}",
+		);
+
+		// Wrapper data attributes
+		$wrapper_data_attrs = array();
+
+		// Column classnames & data attribute
+		if ( in_array( $this->props['layout'], array( 'grid', 'masonry' ) ) ) {
+			array_push( $wrapper_classnames, "sae-gallery-column-{$this->props['column']}" );
+
+			$wrapper_data_attrs['column'] = $this->props['column'];
+		}
+
+		// Rendered attributes
+		$rendered_wrapper_classnames = implode( ' ', $wrapper_classnames );
+		$rendered_data_attrs         = '';
+
+		foreach ( $wrapper_data_attrs as $attr_name => $attr_value ) {
+			$rendered_data_attrs .= sprintf(
+				' data-sae-%1$s="%2$s"',
+				esc_attr( $attr_name ),
+				esc_attr( $attr_value )
+			);
+		}
+
 		return sprintf(
-			'<div class="sae-gallery-wrapper">%1$s</div>',
+			'<div class="%1$s"%2$s>%3$s</div>',
+			esc_attr( $rendered_wrapper_classnames ),
+			et_sanitized_previously( $rendered_data_attrs ),
 			et_sanitized_previously( $this->content )
 		);
 	}
